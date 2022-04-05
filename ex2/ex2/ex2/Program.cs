defdaff709a91eda4bdeab9cd6d9a6e048a4059f
pedro7161 <pedro.ramos9@hotmail.com>
2022-04-05 09:25:47 +0100

    delegtes and lambda

--- ex2/ex2/ex2/Program.cs
+++ ex2/ex2/ex2/Program.cs
@@ -15,6 +15,22 @@ namespace ex2
             int resul = biblio.multiplica(5, 4);
             Console.WriteLine(resul);
             Console.WriteLine(biblio.multiplica(7));
+            biblio ola = new biblio();
+            int x = 1, y = 2;
+            Console.WriteLine("x={0} y={1}",x,y);
+            ola.troca(ref x, ref y);
+            Console.WriteLine("x={0} y={1}", x, y);
+            int res;
+            float med;
+            biblio.somas(x,y,out res,out med);
+            //namedlg func = biblio.soma;
+            namedlg func = new namedlg(biblio.soma);
+            Console.WriteLine(func(2,3));
+            func = delegate (int a, int b) { return a * b; };
+            Console.WriteLine(func(2, 3));
+            func = (a, b) => a - b;
+            Console.WriteLine(func(2, 3));
+
             Console.ReadLine();
         }
     }
--- ex2/ex2/ex2/biblio.cs
+++ ex2/ex2/ex2/biblio.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace ex2
 {
+    public delegate int namedlg(int a, int b);
     class biblio
     {
+        
+        public static int soma(int x, int y)
+        {
+            return x + y;
+        }
         public static int multiplica(int x, int fator = 2)
         {
             return x * fator;
@@ -29,6 +35,17 @@ namespace ex2
                 soma += nums[i];
             }
             return soma;
+        }
+        public void troca(ref int x,ref int y) {
+            int pivo = x;
+            x = y;
+            y = pivo;
+    
+        }
+        public static void somas(int a,int b, out int rslt,out float media)
+        {
+            rslt = a + b;
+            media = (float)rslt / 2.0f;
         }
     }
 }
